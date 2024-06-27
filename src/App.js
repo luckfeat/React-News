0d2b4574560779df02859315e46a1d007011a89b
luckfeat <luckfeat@gmail.com>
2024-06-27 17:59:25 +0900

    feat: Add react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { useCallback, useState } from "react";
 import NewsList from "./components/NewsList";
 import Categories from "./components/Categories";
+import { Route, Routes } from "react-router-dom";
+import NewsPage from "./pages/NewsPage";
 
 const App = () => {
-    const [category, setCategory] = useState("all");
-    const onSelect = useCallback((category) => setCategory(category), []);
-
     return (
-        <div>
-            <Categories category={category} onSelect={onSelect} />
-            <NewsList category={category} />
-        </div>
+        <Routes>
+            <Route path="/" element={<NewsPage />} />
+            <Route path="/:category" element={<NewsPage />} />
+        </Routes>
     );
 };
 
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled, { css } from "styled-components";
+import { NavLink } from "react-router-dom";
 
 const categories = [
     { name: "all", text: "전체보기" },
@@ -22,7 +23,7 @@ const CategoriesBlock = styled.div`
     }
 `;
 
-const Category = styled.div`
+const Category = styled(NavLink)`
     font-size: 1.125rem;
     cursor: pointer;
     white-space: pre;
@@ -34,16 +35,14 @@ const Category = styled.div`
         color: #495057;
     }
 
-    ${(props) =>
-        props.active &&
-        css`
-            font-weight: 600;
-            border-bottom: 2px solid #22b8cf;
-            color: #22b8cf;
-            &:hover {
-                color: #3bc9db;
-            }
-        `}
+    &.active {
+        font-weight: 600;
+        border-bottom: 2px solid #22b8cf;
+        color: #22b8cf;
+        &:hover {
+            color: #3bc9db;
+        }
+    }
 
     & + & {
         margin-left: 1rem;
@@ -56,8 +55,10 @@ const Categories = ({ onSelect, category }) => {
             {categories.map((c) => (
                 <Category
                     key={c.name}
-                    active={category === c.name}
-                    onClick={() => onSelect(c.name)}
+                    className={({ isActive }) =>
+                        isActive ? "active" : undefined
+                    }
+                    to={c.name === "all" ? "/" : `/${c.name}`}
                 >
                     {c.text}
                 </Category>
